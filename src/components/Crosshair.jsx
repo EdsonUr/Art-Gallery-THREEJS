186ea29eeaa2f4c4db77c66244129c6a00447e06
Edson <e.uranoc@gmail.com>
2025-02-14 10:14:56 -0300

    fix: show crosshair only when PointerLockControls is active

--- src/components/Crosshair.jsx
+++ src/components/Crosshair.jsx
@@ -1,23 +1,38 @@
+import { useState, useEffect } from "react";
+
 const Crosshair = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const handleLockChange = () => {
+      setVisible(document.pointerLockElement !== null);
+    };
+
+    document.addEventListener("pointerlockchange", handleLockChange);
+    return () => document.removeEventListener("pointerlockchange", handleLockChange);
+  }, []);
+
+  if (!visible) return null;
+
   return (
     <div
+      className="
+        pointer-events-none
+        absolute
+        top-1/2 left-1/2
+        -translate-x-1/2 -translate-y-1/2
+      "
+    >
+      <div
         className="
-          pointer-events-none
-          absolute
-          top-1/2 left-1/2
-          -translate-x-1/2 -translate-y-1/2
+          w-3 h-3
+          rounded-full
+          border border-white
+          bg-transparent
         "
-      >
-        <div
-          className="
-            w-3 h-3
-            rounded-full
-            border-1 border-white
-            bg-transparent
-          "
-        />
-      </div>
-  )
-}
+      />
+    </div>
+  );
+};
 
-export default Crosshair
+export default Crosshair;
